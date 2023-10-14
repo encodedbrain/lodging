132afea652f20e182ea7d30dd29ddc5ecc46d950
marcoDmc <marcodamasceno07@gmail.com>
2023-10-14 02:05:42 -0300

    feat: added new services

--- Services/Reserve/ShowGuestsService.cs
+++ Services/Reserve/ShowGuestsService.cs
@@ -1,6 +1,37 @@
-﻿namespace lodging.Services;
+﻿using lodging.Data;
+using lodging.Schemas;
+
+namespace lodging.Services;
 
 public class ShowGuestsService
 {
-    
+    public async Task<object> ShowGuests(ShowGuestsSchema prop)
+    {
+        using (var context = new LodgingDb())
+        {
+            var method = new ValidateCredentialsService();
+            if (!ValidateCredentials(prop, method)) return false;
+
+            var userExists = context.Persons.FirstOrDefault(person => person.Password == method.EncryptingPassword(prop.Password)
+            && person.Email == prop.Email);
+
+            if (userExists is null) return false;
+
+            var guests = context.Reserves.FirstOrDefault(reserve => reserve.ReserveId == userExists.Id);
+
+            if (guests is null) return false;
+
+            return new
+            {
+                adults = guests.Adults,
+                childrens = guests.Childrens
+            };
+        }
+    }
+    private bool ValidateCredentials(ShowGuestsSchema prop, ValidateCredentialsService verifyCredentials)
+    {
+        if (!verifyCredentials.VaLidateEmail(prop.Email)) return false;
+        if (!verifyCredentials.ValidatePassword(prop.Password)) return false;
+        return true;
+    }
 }
